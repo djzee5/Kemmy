5dadbe2b09d85dc012922b12af21609feed706f8
djzee5 <113630697+djzee5@users.noreply.github.com>
2022-11-20 22:41:51 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,12 +5,30 @@
 
 */
 
+  //Global variables
+
+  var video = document.getElementById("example");
+  var videoSource =  document.getElementById("vid-src");
+
+  //function to display the advert video
+  function advert() {
+     videoSource.src = "/kemmy/media/food.mp4";
+     video.style.display = "block";
+     video.load();
+
+      video.addEventListener('ended', function() {
+          // hide video
+            video.style.display = "none";
+          })
+     }
+
+
   //Hamburger menu function
 
   function hamburger() {
       var menu = document.getElementById("menu-links");
       var logo = document.getElementById("ffc-logo");
-      if (menu.style.display === 'block' && logo.style.display === 'none') {
+      if (menu.style.display === "block" && logo.style.display === "none") {
           menu.style.display = 'none';
           logo.style.display = 'block';
       }
@@ -20,3 +38,4 @@
       }
 
   }
+
